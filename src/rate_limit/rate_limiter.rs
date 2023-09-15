a78d86642bbb62cb440874f37719c2e658b90d26
Phill <philltv.gaming@gmail.com>
2023-09-16 01:07:10 +0200

    feat: add RateLimiter struct

--- src/rate_limit/rate_limiter.rs
+++ src/rate_limit/rate_limiter.rs
@@ -1,13 +1,65 @@
-use std::net::IpAddr;
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::net::SocketAddr;
+use std::ops::Add;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
-pub struct RateLimiter {}
+// A basic rate limiter implementation.
+#[derive(Clone, Debug)]
+pub struct RateLimiter {
+    ip_counts: Arc<Mutex<HashMap<SocketAddr, u32>>>, // IP address to (request count, last request time)
+    max_requests: u32,                               // Maximum requests allowed per minute
+    reset_duration: Duration,                        // Duration for resetting request counts
+}
+
+impl Default for RateLimiter {
+    fn default() -> Self {
+        Self {
+            reset_duration: Duration::from_secs(60),
+            max_requests: 10, // 100
+            ip_counts: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
 
 impl RateLimiter {
-    fn new() -> Self {
-        todo!()
+    pub fn new(max_requests: u32, reset_duration: Duration) -> Self {
+        let limiter = Self {
+            ip_counts: Arc::new(Mutex::new(HashMap::new())),
+            max_requests,
+            reset_duration,
+        };
+        log::info!("RateLimiter initialized!");
+
+        let l = limiter.clone();
+        std::thread::spawn(move || {
+            println!("{:?} running RateLimiter", std::thread::current().id());
+
+            loop {
+                // Do things..
+                l.ip_counts.clone().lock().unwrap().clear();
+
+                std::thread::sleep(limiter.reset_duration);
+                println!("RateLimiter reset...");
+            }
+        });
+
+        limiter
     }
 
-    fn is_allowed(&self, ip: IpAddr) -> bool {
-        todo!()
+    pub fn check_rate_limit(&mut self, ip: SocketAddr) -> bool {
+        if let Ok(mut lock) = self.ip_counts.lock() {
+            if let Some(requests) = lock.get_mut(&ip) {
+                *requests += 1;
+
+                return requests < &mut self.max_requests;
+            } else {
+                lock.insert(ip, 1);
+                return true;
+            }
+        }
+
+        return false;
     }
 }
