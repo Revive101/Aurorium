8e34c3dd42fab60ae64e0c9a3baaf180cf5f5f9d
Phill <philltv.gaming@gmail.com>
2023-10-14 21:32:04 +0200

    feat: add disable_ratelimit to struct

--- src/rate_limit/rate_limiter.rs
+++ src/rate_limit/rate_limiter.rs
@@ -9,6 +9,7 @@ pub struct RateLimiter {
     ip_counts: Arc<Mutex<HashMap<SocketAddr, u32>>>, // IP address & request count
     max_requests: u32,                               // Maximum requests allowed per minute
     reset_duration: Duration,                        // Duration for resetting request counts
+    disabled: bool,                                  // If the RateLimiter is disabled
 }
 
 impl Default for RateLimiter {
@@ -17,36 +18,42 @@ impl Default for RateLimiter {
             reset_duration: Duration::from_secs(60),
             max_requests: 100, // 100
             ip_counts: Arc::new(Mutex::new(HashMap::new())),
+            disabled: false,
         }
     }
 }
 
 impl RateLimiter {
-    pub fn new(max_requests: u32, reset_duration: Duration) -> Self {
+    pub fn new(max_requests: u32, reset_duration: Duration, disabled: bool) -> Self {
         let limiter = Self {
             ip_counts: Arc::new(Mutex::new(HashMap::new())),
             max_requests,
             reset_duration,
+            disabled,
         };
-        log::info!("RateLimiter initialized!");
 
-        let l = limiter.clone();
-        std::thread::spawn(move || {
-            log::info!("{:?} running RateLimiter", std::thread::current().id());
+        if !disabled {
+            log::info!("RateLimiter initialized!");
+            let l = limiter.clone();
+            std::thread::spawn(move || {
+                loop {
+                    // Do things..
+                    l.ip_counts.clone().lock().unwrap().clear();
 
-            loop {
-                // Do things..
-                l.ip_counts.clone().lock().unwrap().clear();
-
-                std::thread::sleep(limiter.reset_duration);
-                log::trace!("RateLimiter reset...");
-            }
-        });
+                    std::thread::sleep(limiter.reset_duration);
+                    log::trace!("RateLimiter reset...");
+                }
+            });
+        }
 
         limiter
     }
 
     pub fn check_rate_limit(&mut self, ip: SocketAddr) -> bool {
+        if self.disabled {
+            return true;
+        }
+
         if let Ok(mut lock) = self.ip_counts.lock() {
             if let Some(requests) = lock.get_mut(&ip) {
                 *requests += 1;
