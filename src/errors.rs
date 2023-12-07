c797f72695db56394a9cef5af2a05daea5ea8823
Phill <philltv.gaming@gmail.com>
2023-12-08 00:18:50 +0100

    chore: rename RevisionCheckerError => RevisionError

--- src/errors.rs
+++ src/errors.rs
@@ -1,13 +1,11 @@
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum RevisionCheckerError {
+pub enum RevisionError {
     #[error("IO Error while reading/writing TcpStream")]
     IO(#[from] std::io::Error),
     #[error("Received invalid MagicHeader sequence")]
     InvalidMagicHeader,
-    #[error(
-        "Expected 'PATCH_SERVICE_ID' (8) PROTOCOL & MSG_LATEST_FILE_LIST_V2 (2) but got {0} & {1}"
-    )]
+    #[error("Expected SERVICE_ID (8) & MESSAGE_ID (2) but got {0} & {1}")]
     InvalidProtocol(u8, u8),
 }
