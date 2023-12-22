bd7c604c21d58f1a0b2e599118064f86e3d29fc9
Phill <philltv.gaming@gmail.com>
2023-12-22 18:38:28 +0100

    chore: remove comments

--- src/main.rs
+++ src/main.rs
@@ -118,12 +118,6 @@ async fn main() {
         Ok(_) => (),
         Err(why) => log::error!("Could not start Axum server! {}", why),
     }
-
-    /* 3 Threads:
-        1. Fileserver
-        2. Ratelimiter
-        3. Fetcher
-    */
 }
 
 async fn check_revision(concurrent_downloads: usize) {
