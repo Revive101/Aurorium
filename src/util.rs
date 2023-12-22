0c2c20309d8b65104b532e6c8605f707d75de5a6
Phill <50775241+Phill030@users.noreply.github.com>
2023-12-22 09:56:03 +0100

    docs: remove comments

--- src/util.rs
+++ src/util.rs
@@ -36,19 +36,17 @@ pub enum Endianness {
 }
 
 pub fn hex_decode(hex_string: &str, endianness: &Endianness) -> Option<Vec<u8>> {
-    // Check if the hex string is a valid length
     if hex_string.len() % 2 != 0 {
         return None;
     }
 
-    // Iterate over pairs of characters and convert them to u8
     let bytes: Option<Vec<u8>> = (0..hex_string.len())
         .step_by(2)
         .map(|i| {
             let byte = u8::from_str_radix(&hex_string[i..i + 2], 16).ok()?;
             match endianness {
                 Endianness::Little => Some(byte),
-                Endianness::Big => Some(byte.reverse_bits()), // For Big Endian, reverse the bits
+                Endianness::Big => Some(byte.reverse_bits()),
             }
         })
         .collect();
