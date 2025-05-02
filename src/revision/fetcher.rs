e717a63bf0451e35bfd034056f32d95ed7a19fcc
Phill <50775241+Phill030@users.noreply.github.com>
2025-05-02 03:19:36 +0200

    ref: use better method to retrieve files

--- src/revision/fetcher.rs
+++ src/revision/fetcher.rs
@@ -4,6 +4,7 @@ use futures_util::{
     StreamExt,
     stream::{self},
 };
+use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Client;
 use roxmltree::{Document, Node};
 use std::{num::NonZeroUsize, path::PathBuf};
@@ -31,7 +32,7 @@ struct AssetList {
     pub utils: Vec<Asset>,
 }
 
-/// Handles fetching and managing game assets from a remote server
+/// Handles fetching and managing game assets
 pub struct AssetFetcher {
     pub list_file_url: String,
     pub url_prefix: String,
@@ -39,51 +40,47 @@ pub struct AssetFetcher {
     assets: AssetList,
     save_directory: PathBuf,
     concurrent_downloads: NonZeroUsize,
+    client: Client,
 }
 
 impl AssetFetcher {
     pub fn new(revision: Revision, concurrent_downloads: NonZeroUsize, save_directory_name: PathBuf) -> Self {
-        let save_directory = save_directory_name.join(&revision.revision);
-
         Self {
             assets: AssetList::default(),
             revision: revision.revision.clone(),
             url_prefix: revision.url_prefix,
             list_file_url: revision.list_file_url,
-            save_directory,
+            save_directory: save_directory_name.join(&revision.revision),
             concurrent_downloads,
+            client: Client::builder().user_agent("KingsIsle Patcher").build().unwrap(),
         }
     }
 
     pub async fn fetch_index(&mut self) -> Result<(), AssetFetcherError> {
+        println!("Fetching LatestFileList...");
         // Fetches the BIN version of `LatestFileList`
-        let list_file_path = self.save_directory.join("LatestFileList.bin");
-        self.download_and_save(&self.list_file_url, &list_file_path).await?;
+        let bin_path = self.save_directory.join("LatestFileList.bin");
+        let response = self.client.get(&self.list_file_url).send().await?;
+        Self::write_to_file_chunked(&bin_path, response).await?;
 
         // Fetches the XML version of `LatestFileList`
-        let xml_url = self.list_file_url.replace("LatestFileList.bin", "LatestFileList.xml");
+        let xml_url = self.list_file_url.replace(".bin", ".xml");
         let xml_path = self.save_directory.join("LatestFileList.xml");
 
-        match self.fetch_and_parse_xml(&xml_url, &xml_path).await {
-            Ok(_) => {
-                self.fetch_files(&self.assets.wads, &self.save_directory).await;
-                self.fetch_files(&self.assets.utils, &self.save_directory).await;
-            }
-            Err(e) => {
-                println!("Failed to parse XML: {e}");
-            }
+        if let Ok(_) = self.process_xml(&xml_url, &xml_path).await {
+            self.fetch_files(&self.assets.wads, &self.save_directory).await;
+            self.fetch_files(&self.assets.utils, &self.save_directory).await;
         }
 
         Ok(())
     }
 
-    //
-    async fn fetch_and_parse_xml(&mut self, url: &str, save_path: &PathBuf) -> Result<(), AssetFetcherError> {
-        let response = Self::send_request(url).await?;
+    async fn process_xml(&mut self, url: &str, save_path: &PathBuf) -> Result<(), AssetFetcherError> {
+        let response = self.client.get(url).send().await?;
         let xml_text = response.text().await.unwrap_or_default();
 
         let sanitized_content = self.sanitize_content(&xml_text).await?;
-        self.propogate_file_list(&sanitized_content);
+        self.propogate_file_list(&sanitized_content)?;
 
         if !save_path.exists() {
             Self::write_to_file(&save_path, &sanitized_content.into_bytes()).await?;
@@ -92,22 +89,14 @@ impl AssetFetcher {
         Ok(())
     }
 
-    fn propogate_file_list(&mut self, xml_text: &str) {
-        let records = parse_xml(xml_text).unwrap();
+    fn propogate_file_list(&mut self, xml_text: &str) -> Result<(), AssetFetcherError> {
+        let records = parse_xml(xml_text)?;
+        let (wads, utils) = records.into_iter().partition(|f| f.filename.ends_with(".wad"));
 
-        let mut wads = Vec::new();
-        let mut utils = Vec::new();
+        self.assets.wads = wads;
+        self.assets.utils = utils;
 
-        for file in records {
-            if file.filename.ends_with(".wad") {
-                wads.push(file);
-            } else {
-                utils.push(file);
-            }
-        }
-
-        self.assets.wads = std::mem::take(&mut wads);
-        self.assets.utils = std::mem::take(&mut utils);
+        Ok(())
     }
 
     // ts looks so ugly like fr 🥀🥀
@@ -120,14 +109,9 @@ impl AssetFetcher {
         output.push_str("<?xml version=\"1.0\" ?>\n<LatestFileList>\n");
 
         for child in root.children() {
-            match child.tag_name().name() {
-                "_TableList" | "About" => continue,
-                _ => {
-                    if child.is_element() {
-                        output.push_str(&Self::node_to_string(child));
-                        output.push('\n');
-                    }
-                }
+            if child.is_element() && !matches!(child.tag_name().name(), "_TableList" | "About") {
+                output.push_str(&Self::node_to_string(child));
+                output.push('\n');
             }
         }
 
@@ -140,10 +124,10 @@ impl AssetFetcher {
         s.push_str(&format!("<{}>", node.tag_name().name()));
 
         for child in node.children() {
-            if child.is_element() {
-                s.push_str(&Self::node_to_string(child));
-            } else if child.is_text() {
-                s.push_str(child.text().unwrap_or(""));
+            match () {
+                _ if child.is_element() => s.push_str(&Self::node_to_string(child)),
+                _ if child.is_text() => s.push_str(child.text().unwrap_or("")),
+                _ => (),
             }
         }
 
@@ -154,50 +138,46 @@ impl AssetFetcher {
 
     // This function starts `n` parallel tasks to fetch multiple files
     async fn fetch_files(&self, file_list: &Vec<Asset>, base_path: &PathBuf) {
+        let style = ProgressStyle::with_template("{spinner:.blue} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len}")
+            .unwrap()
+            .progress_chars("#>-");
+
+        let pb = ProgressBar::new(file_list.len() as u64);
+        pb.set_style(style);
+
         stream::iter(file_list.iter().map(|file| {
             let url = format!("{}/{}", self.url_prefix, file.filename);
             let path = base_path.join(&file.filename);
 
-            async move {
-                if !path.exists() {
-                    match Self::send_request(&url).await {
-                        Ok(res) => match Self::write_to_file_chunked(&path, res).await {
-                            Err(e) => println!("[❌] Could not fetch {}: {e}", file.filename),
-                            _ => {
-                                println!("[✔] Fetched {}", file.filename);
-                            }
-                        },
-                        Err(e) => println!("[❌] Could not fetch {}: {}", file.filename, e),
+            {
+                let progress_bar = pb.clone();
+                async move {
+                    if !path.exists() {
+                        match self.client.get(&url).send().await {
+                            Ok(res) => match Self::write_to_file_chunked(&path, res).await {
+                                Err(e) => println!("[❌] Could not fetch {}: {e}", file.filename),
+                                _ => {}
+                            },
+                            Err(e) => println!("[❌] Could not fetch {}: {}", file.filename, e),
+                        }
                     }
-                } else {
-                    println!("Skipping {:?}", &file.filename)
+
+                    progress_bar.inc(1);
                 }
             }
         }))
         .buffer_unordered(self.concurrent_downloads.get())
         .collect::<Vec<_>>()
         .await;
-    }
 
-    async fn download_and_save(&self, url: &str, path: &PathBuf) -> std::io::Result<()> {
-        if path.exists() {
-            // log::info!("File already exists, skipping download: {:?}", path);
-            return Ok(());
-        }
-
-        let response = Self::send_request(url).await.unwrap();
-        Self::write_to_file(path, &response.bytes().await.unwrap()).await
-    }
-
-    async fn send_request(url: &str) -> Result<reqwest::Response, reqwest::Error> {
-        let client = Client::new();
-        client.get(url).header("User-Agent", "KingsIsle Patcher").send().await
+        pb.finish();
     }
 
     async fn write_to_file(path: &PathBuf, content: &[u8]) -> std::io::Result<()> {
         if let Some(parent) = path.parent() {
             create_dir_all(parent).await?;
         }
+
         write(path, content).await
     }
 
