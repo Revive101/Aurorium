db7b95e263bb5039d51b6860987aa4c6229d9ed2
Phill <50775241+Phill030@users.noreply.github.com>
2025-05-02 16:44:06 +0200

    chore: move files

--- src/utils.rs
+++ src/utils.rs
@@ -4,6 +4,30 @@ use axum::{
 };
 use std::net::SocketAddr;
 
+pub enum Endianness {
+    Little,
+    Big,
+}
+
+pub fn hex_decode(hex_string: &str, endianness: &Endianness) -> Option<Vec<u8>> {
+    if hex_string.len() % 2 != 0 {
+        return None;
+    }
+
+    let bytes: Option<Vec<u8>> = (0..hex_string.len())
+        .step_by(2)
+        .map(|i| {
+            let byte = u8::from_str_radix(&hex_string[i..i + 2], 16).ok()?;
+            match endianness {
+                Endianness::Little => Some(byte),
+                Endianness::Big => Some(byte.reverse_bits()),
+            }
+        })
+        .collect();
+
+    bytes
+}
+
 #[derive(Debug)]
 pub struct ConnectionAddr(pub String);
 
