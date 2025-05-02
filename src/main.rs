598a12b9f1e51ef127ec86ae8f2f6b6ec532516a
Phill <50775241+Phill030@users.noreply.github.com>
2025-05-02 03:17:47 +0200

    ref: use NonZeroUsize and add arguments for concurrency and save directory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use axum::{extract::Path, response::IntoResponse};
 use clap::Parser;
 use extract::ConnectionAddr;
+use indicatif::{ProgressBar, ProgressStyle};
 use revision::{Revision, fetcher::AssetFetcher};
 use std::{
+    error::Error,
     net::{Ipv4Addr, SocketAddrV4},
-    num::NonZero,
+    num::NonZeroUsize,
     path::PathBuf,
 };
 
@@ -16,19 +18,23 @@ pub mod util;
 #[derive(Clone, Parser)]
 struct Args {
     #[arg(short, long, default_value_t = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 12369))]
-    /// Socket address to listen
     endpoint: SocketAddrV4,
+
+    #[arg(short, long, default_value_t = unsafe { NonZeroUsize::new_unchecked(1) })]
+    concurrent_downloads: NonZeroUsize,
+
+    #[arg(short, long, default_value = "data")]
+    save_directory: PathBuf,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     let revision = Revision::check().await.unwrap();
-    AssetFetcher::new(revision, NonZero::new(1).unwrap(), PathBuf::from("data"))
+    AssetFetcher::new(revision, args.concurrent_downloads, args.save_directory)
         .fetch_index()
-        .await
-        .unwrap(); // TODO: remove unwrap
+        .await?;
 
     // let file_serving = spawn(async move {
     //     let router = Router::new()
@@ -38,6 +44,8 @@ async fn main() {
     //     let listener = TcpListener::bind(&args.endpoint).await.unwrap();
     //     if let Err(why) = axum::serve(listener, router.into_make_service_with_connect_info::<SocketAddr>()).await {}
     // });
+
+    Ok(())
 }
 
 pub async fn file(Path((revision, file_path)): Path<(String, String)>, ConnectionAddr(addr): ConnectionAddr) -> impl IntoResponse {
