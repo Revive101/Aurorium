94b2f5cafc83bab22f8ba2ffed802c110fc6d568
Phill <50775241+Phill030@users.noreply.github.com>
2025-05-06 04:10:33 +0200

    ref: fix stupid `implementation is not general enough` Error & add MultiProgressBar

--- src/fetcher/client.rs
+++ src/fetcher/client.rs
@@ -8,9 +8,12 @@ use futures_util::{
     StreamExt,
     stream::{self},
 };
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use reqwest::Client;
-use std::{num::NonZeroUsize, path::PathBuf};
+use std::{
+    num::NonZeroUsize,
+    path::{Path, PathBuf},
+};
 use tokio::{
     fs::{File, create_dir_all, write},
     io::AsyncWriteExt,
@@ -21,42 +24,44 @@ pub struct AssetFetcher {
     pub list_file_url: String,
     pub url_prefix: String,
     pub revision: String,
-    assets: AssetList,
+    pub assets: AssetList,
     save_directory: PathBuf,
     concurrent_downloads: NonZeroUsize,
     client: Client,
 }
 
 impl AssetFetcher {
-    pub fn new(revision: PatchInfo, concurrent_downloads: NonZeroUsize, save_directory_name: PathBuf) -> Self {
+    pub fn new<P>(patch_info: &PatchInfo, concurrent_downloads: NonZeroUsize, save_directory_name: P) -> Self
+    where
+        P: AsRef<Path>,
+    {
         Self {
             assets: AssetList::default(),
-            revision: revision.revision.clone(),
-            url_prefix: revision.url_prefix,
-            list_file_url: revision.list_file_url,
-            save_directory: save_directory_name.join(&revision.revision),
+            revision: patch_info.revision.clone(),
+            url_prefix: patch_info.url_prefix.clone(),
+            list_file_url: patch_info.list_file_url.clone(),
+            save_directory: save_directory_name.as_ref().join(&patch_info.revision),
             concurrent_downloads,
             client: Client::builder().user_agent("KingsIsle Patcher").build().unwrap(),
         }
     }
 
-    pub async fn fetch_index(&mut self) -> Result<(), AssetFetcherError> {
+    pub async fn fetch_index(&mut self) -> Result<&mut Self, AssetFetcherError> {
         println!("Fetching LatestFileList...");
         // Fetches the BIN version of `LatestFileList`
         let bin_path = self.save_directory.join("LatestFileList.bin");
-        let response = self.client.get(&self.list_file_url).send().await?;
-        Self::write_to_file_chunked(&bin_path, response).await?;
+        if !bin_path.exists() {
+            let response = self.client.get(&self.list_file_url).send().await?;
+            Self::write_to_file_chunked(&bin_path, response).await?;
+        }
 
         // Fetches the XML version of `LatestFileList`
         let xml_url = self.list_file_url.replace(".bin", ".xml");
         let xml_path = self.save_directory.join("LatestFileList.xml");
 
-        if let Ok(_) = self.process_xml(&xml_url, &xml_path).await {
-            self.fetch_files(&self.assets.wads, &self.save_directory).await;
-            self.fetch_files(&self.assets.utils, &self.save_directory).await;
-        }
+        self.process_xml(&xml_url, &xml_path).await?;
 
-        Ok(())
+        Ok(self)
     }
 
     async fn process_xml(&mut self, url: &str, save_path: &PathBuf) -> Result<(), AssetFetcherError> {
@@ -78,43 +83,65 @@ impl AssetFetcher {
     }
 
     // This function starts `n` parallel tasks to fetch multiple files
-    async fn fetch_files(&self, file_list: &Vec<Asset>, base_path: &PathBuf) {
+    pub async fn fetch_files(&self, file_list: Vec<Asset>) {
         println!("Fetching files...");
 
-        let style = ProgressStyle::with_template("{spinner:.blue} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len}")
+        let multi_pb = MultiProgress::new();
+
+        let main_style = ProgressStyle::with_template("{spinner:.blue} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len}")
             .unwrap()
             .progress_chars("#>-");
 
-        let pb = ProgressBar::new(file_list.len() as u64);
-        pb.set_style(style);
+        let main_pb = multi_pb.add(ProgressBar::new(file_list.len() as u64));
+        main_pb.set_style(main_style);
 
-        stream::iter(file_list.iter().map(|file| {
-            let url = format!("{}/{}", self.url_prefix, file.filename);
-            let path = base_path.join(&file.filename);
+        let download_style = ProgressStyle::with_template(
+            "{msg:.cyan} {spinner:.blue} [{elapsed_precise}] [{wide_bar:.green/blue}] {bytes}/{total_bytes} ({eta})",
+        )
+        .unwrap()
+        .progress_chars("#>-");
 
-            {
-                let progress_bar = pb.clone();
-                async move {
-                    if !path.exists() {
-                        match self.client.get(&url).send().await {
-                            Ok(res) => match Self::write_to_file_chunked(&path, res).await {
-                                Err(e) => println!("[❌] Could not write file {}: {e}", file.filename),
-                                _ => {}
-                            },
-                            Err(e) => println!("[❌] Could not fetch {}: {}", file.filename, e),
+        let download_futures = file_list.into_iter().map(|file| {
+            let client = self.client.clone();
+            let url_prefix = self.url_prefix.clone();
+            let save_dir = self.save_directory.clone();
+            // Progress bar
+            let multi_pb = multi_pb.clone();
+            let main_pb = main_pb.clone();
+            let style = download_style.clone();
+
+            async move {
+                let url = format!("{}/{}", url_prefix, file.filename);
+                let path = save_dir.join(&file.filename);
+
+                if !path.exists() {
+                    let file_pb = multi_pb.add(ProgressBar::new_spinner());
+                    file_pb.set_style(style);
+                    file_pb.set_message(format!("Downloading {}", file.filename));
+
+                    if let Ok(res) = client.get(&url).send().await {
+                        file_pb.set_length(res.content_length().unwrap_or(0));
+
+                        if let Err(e) = Self::write_to_file_chunked_with_progress(&path, res, &file_pb).await {
+                            file_pb.finish_with_message(format!("Failed: {}", e));
+                        } else {
+                            file_pb.finish_with_message("Done");
                         }
+
+                        multi_pb.remove(&file_pb);
                     }
-
-                    progress_bar.inc(1);
                 }
-            }
-        }))
-        .buffer_unordered(self.concurrent_downloads.get())
-        .collect::<Vec<_>>()
-        .await;
 
-        pb.finish();
-        println!("Done!");
+                main_pb.inc(1);
+            }
+        });
+
+        stream::iter(download_futures)
+            .buffer_unordered(self.concurrent_downloads.get())
+            .collect::<Vec<()>>()
+            .await;
+
+        main_pb.finish_with_message("All downloads complete.");
     }
 
     async fn write_to_file(path: &PathBuf, content: &[u8]) -> std::io::Result<()> {
@@ -133,6 +160,24 @@ impl AssetFetcher {
         let mut file = File::create(path).await?;
         while let Some(chunk) = response.chunk().await.unwrap() {
             file.write_all(&chunk).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn write_to_file_chunked_with_progress(
+        path: &std::path::Path,
+        mut response: reqwest::Response,
+        pb: &ProgressBar,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        if let Some(parent) = path.parent() {
+            create_dir_all(parent).await?;
+        }
+
+        let mut file = File::create(path).await?;
+        while let Some(chunk) = response.chunk().await.unwrap() {
+            file.write_all(&chunk).await?;
+            pb.inc(chunk.len() as u64);
         }
 
         Ok(())
