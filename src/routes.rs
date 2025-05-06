b40441cbabd9415c9c59068b5b57ba60d9391021
Phill <50775241+Phill030@users.noreply.github.com>
2025-05-06 04:08:34 +0200

    feat: add routes

---
+++ src/routes.rs
@@ -0,0 +1,42 @@
+use crate::{ARGS, REVISIONS, models::revision::LocalRevision, utils::ConnectionAddr};
+use axum::{
+    body::Body,
+    extract::Path,
+    response::{AppendHeaders, IntoResponse},
+};
+use reqwest::{StatusCode, header};
+use serde_json::json;
+use tokio_util::io::ReaderStream;
+
+pub async fn file(Path((revision, file_path)): Path<(String, String)>, ConnectionAddr(addr): ConnectionAddr) -> impl IntoResponse {
+    if let Some(revision_for_asset) = LocalRevision::find_revision_for_asset(revision, &file_path).await {
+        let path = ARGS.save_directory.join(revision_for_asset).join(file_path);
+        let file = match tokio::fs::File::open(&path).await {
+            Ok(file) => file,
+            Err(_) => return Err((StatusCode::NOT_FOUND, format!("File not found: {path:?}")).into_response()),
+        };
+
+        let file_length = file.metadata().await.map(|meta| meta.len()).unwrap_or(0).to_string();
+        let file_name = path.file_name().unwrap().to_string_lossy();
+        let header_content = format!("attachment; filename=\"{}\"", file_name.to_string());
+        let headers = AppendHeaders([
+            (header::CONTENT_TYPE, "text/plain; charset=utf-8"),
+            (header::CONTENT_DISPOSITION, &header_content),
+            (header::CONTENT_LENGTH, file_length.as_str()),
+        ]);
+
+        let stream = ReaderStream::new(file);
+        let body = Body::from_stream(stream);
+
+        Ok((headers, body).into_response())
+    } else {
+        Err((StatusCode::NOT_FOUND, format!("File not found: {file_path}")).into_response())
+    }
+}
+
+pub async fn revisions(ConnectionAddr(addr): ConnectionAddr) -> impl IntoResponse {
+    let revisions = REVISIONS.read().await.iter().map(|r| r.name.clone()).collect::<Vec<_>>();
+    let headers = AppendHeaders([(header::CONTENT_TYPE, "application/json; charset=utf-8")]);
+
+    (headers, json!(*revisions).to_string()).into_response()
+}
