60d1de6c91fe567b288c69a5ee34218df0f7064e
Phill <50775241+Phill030@users.noreply.github.com>
2026-07-22 03:11:35 +0200

    docs: improve comments

--- src/revision/local_revision.rs
+++ src/revision/local_revision.rs
@@ -1,5 +1,9 @@
+use miette::Severity;
 use regex::Regex;
-use std::{path::PathBuf, sync::LazyLock};
+use std::{
+    path::{Path, PathBuf},
+    sync::LazyLock,
+};
 
 use crate::revision::asset_list::AssetList;
 
@@ -8,10 +12,10 @@ static REVISION_RE: LazyLock<Regex> =
 
 #[derive(Debug)]
 pub struct LocalRevision {
-    /// Full Revision-name (e.g. "V_r773351.Wizard_1_570_0_Live")
+    /// Revision name (e.g., `V_r773351.Wizard_1_570_0_Live`)
     pub name: String,
 
-    /// Numeric revision number for comparison (e.g. 773351)
+    /// Numeric revision number for comparison (e.g., `773351`)
     pub number: u64,
 
     /// Path where this revision is stored
@@ -22,15 +26,23 @@ pub struct LocalRevision {
 }
 
 impl LocalRevision {
-    pub fn new<P>(name: &str, base_path: P, assets: AssetList) -> Option<Self> {
+    pub fn new<P>(name: &str, base_path: P, assets: AssetList) -> miette::Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        Ok(Self {
+            name: name.to_string(),
+            number: Self::extract_revision_number(name)?,
+            path: base_path.as_ref().join(name),
+            assets,
+        })
+    }
+
+    pub async fn load_from_name<P>(name: &str, base_path: P) -> miette::Result<Self> {
         todo!()
     }
 
-    pub async fn load_from_name<P>(name: &str, base_path: P) -> Option<Self> {
-        todo!()
-    }
-
-    pub async fn latest() -> Option<Self> {
+    pub async fn latest() -> miette::Result<Self> {
         todo!()
     }
 
@@ -39,10 +51,23 @@ impl LocalRevision {
     }
 
     // Internal functions
-    fn extract_revision_number(name: &str) -> Option<u64> {
-        let captures = REVISION_RE.captures(name)?;
-        let revision_number = captures.get(1)?.as_str().parse::<u64>().ok()?;
+    fn extract_revision_number(name: &str) -> miette::Result<u64> {
+        if let Some(captures) = REVISION_RE.captures(name).and_then(|c| c.get(1)) {
+            let revision_number = captures.as_str().parse::<u64>().map_err(|e| {
+                miette::miette!(
+                    "Failed to parse revision number from '{}': {}",
+                    name,
+                    e.to_string()
+                )
+            })?;
 
-        Some(revision_number)
+            return Ok(revision_number);
+        }
+
+        Err(miette::miette!(
+            severity = Severity::Error,
+            "Failed to extract revision number from '{}'",
+            name
+        ))
     }
 }
